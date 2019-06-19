e3443934759fc552c11a7389b80ec5792a03ca14
Looana <luanagabrielescosta@gmail.com>
2019-06-19 12:15:29 -0300

    Incrivel

--- Trabalho_ProgII_luana-costa-ana-vervloet/trabalho.c
+++ Trabalho_ProgII_luana-costa-ana-vervloet/trabalho.c
@@ -22,19 +22,25 @@ struct aluno{
     float pf;
 };
 
+// Struct para armazenar alunos que passaram em PB
 struct passei{
     char nome[30];
     float mediaParcial;
     float mediaFinal;
 };
 
+// Declaração de tipo struct
 typedef struct aluno Aluno;
 typedef struct passei Passei;
 
+
+void verificaAprovados(char *nome, float *mP, float *mF, Passei *p){
+    
+}
+
 // Função que calcula a média parcial de um caso
-void mediaParcial(float p1, float p2, float p3, float p4, float mParcial){
-    int i;
-    float menor = 10, soma;
+float mediaParcial(float p1, float p2, float p3, float p4){
+    float mP, menor = 10, soma;
 
 // Soma de todas as notas (incluindo a menor)
     soma = p1 + p2 + p3 + p4;
@@ -58,20 +64,33 @@ void mediaParcial(float p1, float p2, float p3, float p4, float mParcial){
 
     soma = soma - menor;
 
-    mParcial = soma/3;
+    mP = soma/3;
+   
+    // Retorna a média parcial
+    return mP;
 }
 
 // Função que calcula a média final de um caso
-void mediaFinal(float p1, float p2, float p3, float p4, float mP, float pf, float mF){
-    
-    mediaParcial(p1, p2, p3, p4, mP);
+float mediaFinal(float p1, float p2, float p3, float p4, float pf){
+    float mP, mF;
 
+    // Chama a função que calcula a média parcial
+    mP = mediaParcial(p1, p2, p3, p4);
+
+    // Faz o cálculo da média final
     mF = (mP + pf)/2;
+    
+    // Retorna a média final
+    return mF;
 }
 
 // Função que retorna a média parcial e a final
 void medias(float p1, float p2, float p3, float p4, float pf, float *mP, float *mF, int count){
-    mediaFinal(p1, p2, p3, p4, mP[count], pf, mF[count]);
+    // Atribui à posição do contador no momento a media final
+    mF[count] = mediaFinal(p1, p2, p3, p4, pf);
+    // Atribui à posição do contador no momento a media parcial
+    mP[count] = mediaParcial(p1, p2, p3, p4);
+    
 }
 
 
@@ -82,8 +101,8 @@ int main(){
     FILE *entrada, *saida;
     Aluno a;
     Passei p[100];
-    int i, j, m;
-    char nomeEntrada[20];
+    int i, j, m, count = 0;
+    char nomeEntrada[20], nomesPB[30][100];
     float mP[100], mF[100];
 
 // Pedindo as informações pelo teclado
@@ -100,20 +119,60 @@ int main(){
             fscanf(entrada, "%s %f %f %f %f %f", a.nome, &a.p1, &a.p2, &a.p3, &a.p4, &a.pf);
 
             // Teste para garantir o valor das variáveis (apagar)
-            printf("\n%s\n", a.nome);
-            printf("%.2f ", a.p1);
-            printf("%.2f ", a.p2);
-            printf("%.2f ", a.p3);
-            printf("%.2f ", a.p4);
-            printf("%.2f ", a.pf);
+            // printf("\n%s\n", a.nome);
+            // printf("%.2f ", a.p1);
+            // printf("%.2f ", a.p2);
+            // printf("%.2f ", a.p3);
+            // printf("%.2f ", a.p4);
+            // printf("%.2f ", a.pf);
 
-            medias(a.p1, a.p2, a.p3, a.p4, a.pf, mP, mF, j);
+            switch(i){
+                case 0:
+                    count = j;
+                    break;
+                case 1:
+                    count = j + 10;
+                    break;
+                case 2:
+                    count = j + 20;
+                    break;
+                case 3:
+                    count = j + 30;
+                    break;
+                case 4:
+                    count = j + 40;
+                    break;
+                case 5:
+                    count = j + 50;
+                    break;
+                case 6:
+                    count = j + 60;
+                    break;
+                case 7:
+                    count = j +70;
+                    break;
+                case 8:
+                    count = j + 80;
+                    break;   
+                default:
+                    count = j + 90;
+                    break;
+            }
+
+            // Chama a função que calcula a média da posição
+            medias(a.p1, a.p2, a.p3, a.p4, a.pf, mP, mF, count);
+            strcpy(nomesPB[count], a.nome);
         }
 
 
 
     // Fecha o arquivo e vai para o próximo loop
         fclose(entrada);
+    }
+
+    for(i = 0; i < 100; i++){
+        printf("%s:\n", nomesPB[i]);
+        printf("%i mP . %.2f mF . %.2f\n", i, mP[i], mP[i]);
     }
     
     return 0;
@@ -139,4 +198,6 @@ para estudo de ordem alfabética
     void funcao(...){
         mParcial[i] = 10;
     }
+
+teste1.txt teste2.txt teste3.txt teste4.txt teste5.txt teste6.txt teste7.txt teste8.txt teste9.txt teste10.txt
 */
